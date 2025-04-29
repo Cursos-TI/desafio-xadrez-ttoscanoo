be4a8ab879bbd60dcc8f21bc52ef3d00142c9bb0
Túlio Toscano <tulio.at13@gmail.com>
2025-04-29 10:02:32 -0300

    Nível Mestre realizado

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,28 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+//Funções Recursivas do Nível Mestre
+void bispoRecursivo (int passos){
+    if (passos > 0) {
+        printf("Cima Direita\n");
+        bispoRecursivo(passos - 1);
+    }
+}
+
+void torreRecursivo (int passos){
+    if (passos > 0) {
+        printf("Direita\n");
+        torreRecursivo(passos - 1);
+    }
+}
+void rainhaRecursivo (int passos){
+    if (passos > 0) {
+        printf("Esquerda\n");
+        rainhaRecursivo(passos - 1);
+    }
+}
+
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -60,9 +82,33 @@ int main() {
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+  
+    printf("\nMovimentação do Bispo Recursiva:\n");
+    bispoRecursivo(bispoMovimento); //Chamando a função recursiva do bispo com a variável de movimento
+
+    printf("\nMovimentação da Torre Recursiva:\n"); 
+    torreRecursivo(torreMovimento);
+    
+    printf("\nMovimentação da Rainha Recursiva:\n");
+    rainhaRecursivo(rainhaMovimento);
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    printf("\nMovimentação do Cavalo com Loops Aninhados:\n");
+    for (int cima = 0, direita = 0; cima < 2 || direita < 1; ) {
+        while (cima < 2) {
+            printf("Cima\n");
+            cima++;
+            if (direita < 1 && cima == 2) {
+                printf("Direita\n");
+                direita++;
+                break; // encerra após o movimento em L completo
+            }
+            continue;
+        }
+        
+    }
 
     return 0;
 }
